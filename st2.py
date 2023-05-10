5535a20f4fd6dd3185284cbc4c63b365ccb65e12
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2023-05-10 19:51:43 +0200

    Create st2.py

---
+++ st2.py
@@ -0,0 +1,20 @@
+from streamlit import line_chart, slider, audio, text_input
+from math import sin
+from requests import get
+
+p = slider("Megjelenítendő kesesési eredmény száma", 1, 50, 8)
+
+i = text_input("Keresés")
+r = get(f"https://itunes.apple.com/search?limit={p}&term={i}").json()
+
+if (r["results"]):
+    for e in r["results"]:
+        if e["artistName"] and e["trackName"] and e["previewUrl"]:
+            e["artistName"] + ": " + e["trackName"]
+            audio(e["previewUrl"])
+else:
+    "Nincs találat"
+
+l = [[sin((i + 7*j) / 10) + 1 for j in range(1, 10)] for i in range(1, 200)]
+
+line_chart(l)
