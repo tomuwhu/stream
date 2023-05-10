f4a58f33ad867a1a90f6c7061bcaa365b591168e
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-10 22:04:13 +0200

    .

--- st3.py
+++ st3.py
@@ -7,7 +7,7 @@ cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
 
 s1 = st.text_input("Filmcím")
 n1 = st.number_input("Megjelenés éve", min_value=1900, max_value=2023, value=2000, step=1)
-n2 = st.number_input("IMDB Score", min_value=0, max_value=10, value=5.0, step=0.1)
+n2 = st.number_input("IMDB Score", min_value=0.0, max_value=10.0, value=5.0, step=0.1)
 
 if st.button("Rögzít"):
     cur.execute(f"INSERT INTO movie VALUES ('{s1}', {n1}, {n2})")
