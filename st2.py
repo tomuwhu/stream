456de6c09f502501cadf139fbedf6fbadca4c664
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-24 16:44:23 +0200

    .

--- st2.py
+++ st2.py
@@ -22,8 +22,3 @@ else:
 l = [[sin((i + 7*j) / 10) + 1 for j in range(1, 10)] for i in range(1, 200)]
 
 line_chart(l)
-
-"## További példák"
-"[Összevissza mindenféle](https://tomuwhu-stream-st-erqpjs.streamlit.app/)"
-"[SQLite példa](https://tomuwhu-stream-st3-zhmjrw.streamlit.app/)"
-"[A másodfokú egyenlet példa](https://tomuwhu-stream-st4-e2uytb.streamlit.app/)"
