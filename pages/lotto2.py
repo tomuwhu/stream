c6af4d516b0bd51c26a531e7a381a4b22cde918b
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-26 14:49:03 +0200

    .

--- pages/lotto2.py
+++ pages/lotto2.py
@@ -1,3 +1,3 @@
 from random import randrange as egyszam
-s = sorted(sorted(range(1,90), key=lambda x: egyszam(100)/100 - 0.5)[:5])
-s
+l = sorted(sorted(range(1,90), key=lambda x: egyszam(100)/100 - 0.5)[:5])
+l
