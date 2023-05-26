54de3b678bee3c653caf7eebcc656f8643c01544
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-26 15:45:14 +0200

    .

--- pages/priority_queue_class.py
+++ pages/priority_queue_class.py
@@ -1,11 +1,13 @@
 from random import randrange as rr
+from streamlit import table as df
 class PQ:
     __d = []
     __size = 0
+    def __init__(self, key = lambda x: x): self.__kf = key
     def __heapify(self, p):
         t = self.__d
         a = (p - 1) // 2
-        if a >= 0 and t[a] < t[p]:
+        if a >= 0 and self.__kf(t[a]) < self.__kf(t[p]):
             t[a], t[p] = t[p], t[a]
             self.__heapify(a)
     def __hf2(self, p):
@@ -14,8 +16,8 @@ class PQ:
         j = p * 2 + 2
         f = -1
         if b < self.__size: f = b
-        if j < self.__size and t[j] > t[b]: f = j
-        if f > 0 and t[p] < t[f]:
+        if j < self.__size and self.__kf(t[j]) > self.__kf(t[b]): f = j
+        if f > 0 and self.__kf(t[p]) < self.__kf(t[f]):
             t[f], t[p] = t[p], t[f]
             self.__hf2(f)
     def put(self, data):
@@ -30,6 +32,6 @@ class PQ:
             self.__hf2(0)
         else: self.__d = []
         return rv
-pq = PQ()
-for i in range(30): pq.put(rr(100,999))
-[pq.get() for _ in range(30)]
+pq = PQ(key = lambda x: -x["key"])
+for i in range(30): pq.put({"key": rr(100,999), "data": str(rr(10000,99999) ** 2)})
+df([pq.get() for _ in range(30)])
