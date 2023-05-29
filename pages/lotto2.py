dc241b0411dc4dac44661abdd6a0c3607ca5958c
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-29 16:35:43 +0200

    .

--- pages/lotto2.py
+++ pages/lotto2.py
@@ -1,8 +1,14 @@
 from random import randrange as sz
+type = {
+    "ötös": {"n": 5, "l": range(1,90)}, 
+    "hatos": {"n": 6, "l": range(1,45)}
+}
+
+typ = "ötös"
 l = sorted(
-    range(1,90), 
+    type[typ]["l"], 
     key = lambda _: 
         sz(100)/100 - 0.5
-    )[:5]
+    )[:type[typ]["n"]]
 l.sort()
-l
+{i+1: j for i, j in enumerate(l)}
---
+++ pages/lotto3.py
@@ -0,0 +1,3 @@
+from random import sample
+s = sorted(sample(range(1, 90), 5))
+s
